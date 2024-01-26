759688534972db0cda93d6ad4244e70ae71c8a48
Abhishek6973 <108926573+Abhishek6973@users.noreply.github.com>
2024-01-26 18:09:42 +0530

    Update settings.py

--- attendence/settings.py
+++ attendence/settings.py
@@ -185,7 +185,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://ezmark.vercel.app",
-    "http://localhost:5173/"
+    "http://localhost:5173"
     
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
